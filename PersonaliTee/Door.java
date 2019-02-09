ffa3a776073335891d1fd6f0719ee19e5b53da41
gaphilli <gaphilli@csc235x36.ad.calpoly.edu>
2019-02-09 14:29:30 -0800

    Here's my sheet

---
+++ PersonaliTee/Door.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Door here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Door extends AnimatedActor
+{
+    /**
+     * Act - do whatever the Door wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }
+    
+    public void enter()
+    {
+        System.out.println("I hit a door.");
+    }
+}
